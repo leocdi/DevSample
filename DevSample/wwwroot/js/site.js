9c16c274ed879feafbb5eef912ab2e881375ec24
leocdi <leomauv@gmail.com>
2023-11-06 19:22:48 +0100

    theme selector

--- DevSample/wwwroot/js/site.js
+++ DevSample/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let elements = document.querySelectorAll('.themebtn');
+
+let clickEvent = (e) => {
+    let themeClicked = e.target.getAttribute('data-bs-Theme-Value')
+    console.log(themeClicked)
+    let html = document.querySelector('html')
+    html.setAttribute('data-bs-Theme', themeClicked)
+    
+}
+elements.forEach((item) => {
+    item.addEventListener('click', clickEvent)
+});
