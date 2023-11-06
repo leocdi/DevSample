33be0d9641571f6d856b3e7ab8181a436ae3264d
leocdi <leomauv@gmail.com>
2023-11-06 07:30:57 +0100

    exemple tag helper

---
+++ DevSample/TagHelpers/ButtonSize.cs
@@ -0,0 +1,23 @@
+﻿
+
+/// <summary>
+///     Types of button
+/// </summary>
+/// <summary>
+///     Sizes of button
+/// </summary>
+public enum ButtonSize
+{
+    /// <summary>
+    ///     Normal size
+    /// </summary>
+    Normal,
+    /// <summary>
+    ///     A large button
+    /// </summary>
+    Large,
+    /// <summary>
+    ///     A small button
+    /// </summary>
+    Small
+}
---
+++ DevSample/TagHelpers/ButtonType.cs
@@ -0,0 +1,19 @@
+﻿
+/// <summary>
+///     Types of button
+/// </summary>
+public enum ButtonType
+{
+    /// <summary>
+    ///     A regular button
+    /// </summary>
+    Button,
+    /// <summary>
+    ///     A form submit button
+    /// </summary>
+    Submit,
+    /// <summary>
+    ///     A form reset button
+    /// </summary>
+    Reset
+}
